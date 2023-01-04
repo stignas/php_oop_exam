9891744929855a916dca369d5870dc55c4d52c56
Ignas <statkus.online@gmail.com>
2023-01-04 19:55:39 +0200

    project initialization

---
+++ src/Container/DiContainer.php
@@ -0,0 +1,111 @@
+<?php
+
+// Source: https://gist.github.com/MustafaMagdi/2bb27aebf6ab078b1f3e5635c0282fac
+
+namespace EAS\Container;
+
+use Exception, Closure, ReflectionClass;
+
+class DiContainer
+{
+    /**
+     * @var array
+     */
+    protected array $instances = [];
+
+    /**
+     * @param      $abstract
+     * @param null $concrete
+     */
+    public function set($abstract, $concrete = NULL)
+    {
+        if ($concrete === NULL) {
+            $concrete = $abstract;
+        }
+        $this->instances[$abstract] = $concrete;
+    }
+
+    /**
+     * @param       $abstract
+     * @param array $parameters
+     *
+     * @return object|null
+     * @throws Exception
+     */
+    public function get($abstract, array $parameters = [])
+    {
+        // if we don't have it, just register it
+        if (!isset($this->instances[$abstract])) {
+            $this->set($abstract);
+        }
+
+        return $this->resolve($this->instances[$abstract], $parameters);
+    }
+
+    /**
+     * resolve single
+     *
+     * @param $concrete
+     * @param $parameters
+     *
+     * @return mixed|object
+     * @throws Exception
+     */
+    public function resolve($concrete, $parameters)
+    {
+        if ($concrete instanceof Closure) {
+            return $concrete($this, $parameters);
+        }
+
+        $reflector = new ReflectionClass($concrete);
+        // check if class is instantiable
+        if (!$reflector->isInstantiable()) {
+            throw new Exception("Class $concrete is not instantiable");
+        }
+
+        // get class constructor
+        $constructor = $reflector->getConstructor();
+        if (is_null($constructor)) {
+            // get new instance from class
+            return $reflector->newInstance();
+        }
+
+        // get constructor params
+        $parameters   = $constructor->getParameters();
+        $dependencies = $this->getDependencies($parameters);
+
+        // get new instance with dependencies resolved
+        return $reflector->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * get all dependencies resolved
+     *
+     * @param $parameters
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getDependencies($parameters): array
+    {
+        $dependencies = [];
+        foreach ($parameters as $parameter) {
+            // get the type hinted class
+            $dependency = $parameter->getClass();
+            if ($dependency === NULL) {
+                // check if default value for a parameter is available
+                if ($parameter->isDefaultValueAvailable()) {
+                    // get default value of parameter
+                    $dependencies[] = $parameter->getDefaultValue();
+                } else {
+                    throw new Exception("Can not resolve class dependency $parameter->name");
+                }
+            } else {
+                // get dependency resolved
+                $dependencies[] = $this->get($dependency->name);
+            }
+        }
+
+        return $dependencies;
+    }
+}
